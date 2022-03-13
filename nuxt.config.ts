a6a17e89bd2c5498f5961c04c44e7f500f07da05
kazuki-komori <komokazu.nankai@gmail.com>
2022-03-13 17:59:32 +0900

    add: deployment

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,8 @@ import { defineNuxtConfig } from 'nuxt3'
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
-  buildModules: ['@nuxtjs/tailwindcss']
+  buildModules: ['@nuxtjs/tailwindcss'],
+  router: {
+    base: "/my_intro/"
+  }
 })
