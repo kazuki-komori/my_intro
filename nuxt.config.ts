8a200b1eb109230dae90e9553bf2c192815c80bd
kazuki-komori <komokazu.nankai@gmail.com>
2022-03-13 18:14:14 +0900

    remove: ci

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,4 @@ import { defineNuxtConfig } from 'nuxt3'
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
   buildModules: ['@nuxtjs/tailwindcss'],
-  router: {
-    base: "/my_intro/"
-  }
 })
